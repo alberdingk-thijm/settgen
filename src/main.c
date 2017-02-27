a0e5f2e0f21a38be7d3ecdb037ce0f9040f76c0a
Tim Alberdingk Thijm <tim.thijm@gmail.com>
2017-02-27 13:37:00 -0500

    add loop logic to main

--- src/main.c
+++ src/main.c
@@ -2,7 +2,21 @@
 #include <stdio.h>
 
 int main(int argc, char[] argv) {
+    int input;
+    while(1) {
 
+        input = sscanf("%i");
+
+        switch (input) {
+            case 1:
+                break;
+            case 2:
+                return 1;
+                break;
+            default:
+                break;
+        }
+    }
 	return 0;
 }
 
