31239bb634a8be31965ad972e1ed371f0f46bc41
Tim Alberdingk Thijm <tim.thijm@gmail.com>
2017-03-07 11:31:18 -0500

    Add notable func prototypes and more struct data

--- src/people.h
+++ src/people.h
@@ -1,14 +1,27 @@
 // Headers for general people and notables
 
+#include "builder.h"
+
 typedef struct notable {
+    district* home;
     char* name;
     int level;
     cls c;
     int age;
+    int maxage;
     race r;
+    int flags;
     // flags?
     struct notable* next;
 } notable;
+
+enum people_flags {
+    NOTABLE_HEALTHY = 0,
+    NOTABLE_ADVENTURING = 1,
+    NOTABLE_TREASURE = 2,
+    NOTABLE_SICK = 4,
+    NOTABLE_DEAD = 8
+};
 
 typedef enum cls {
     CLERIC,
@@ -35,4 +48,7 @@ typedef enum race {
 } race;
 
 static int notable_kill(notable* tokill);
-static int notable_create(char* name, int lvl, enum race r, enum cls c);
+int notable_create(district* d, char* name, int lvl, race r, cls c);
+int notable_execute_ts(notable* n);
+static int notable_adventure(notable* n);
+static int notable_find_treasure(notable* n);
